0324c5fb102ab276212851150851fc784c995277
Linda <mcgowlc@live.com>
2019-10-10 08:56:16 -0400

    added create, edit, delete

--- ice_cream/views.py
+++ ice_cream/views.py
@@ -17,18 +17,35 @@ class DetailsFlavorView(DetailView):
 class DailyFlavorView(ListView):
    model = IceCream
    template_name = 'daily_flavors.html'
-
+queryset = IceCream.objects.filter(available='daily')
 
 class WeeklyFlavorView(ListView):
    model = IceCream
    template_name = 'weekly_flavors.html'
-
+queryset = IceCream.objects.filter(available='weekly')
 
 
 class SeasonalFlavorView(ListView):
    model = IceCream
    template_name = 'seasonal_flavors.html'
+   queryset = IceCream.objects.filter(available='seasonal')
 
 class FeaturedFlavorView(ListView):
     model = IceCream
     template_name = 'home.html'
+
+class FlavorCreateView(CreateView):
+   model = IceCream
+   template_name = 'create.html'
+   fields = ['flavor', 'url', 'base', 'available', 'featured', 'date_churned']
+
+   class FlavorEditView(UpdateView):
+   model = IceCream
+   template_name = 'edit.html'
+   fields = ['flavor', 'url', 'base', 'available', 'featured', 'date_churned']
+
+
+class FlavorDeleteView(DeleteView):
+   model = IceCream
+   template_name = 'delete.html'
+   success_url = reverse_lazy('home')
